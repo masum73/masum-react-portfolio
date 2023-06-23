7088cb1eda3c3fd6a1c80bbd13a4de2eb8071d68
Masum Rahman <masumrahman73@gmail.com>
2023-06-23 12:12:35 +0600

    initial project setup done, react router, tailwind , material tailwind installed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@material-tailwind/react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Button>Button</Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { ThemeProvider } from "@material-tailwind/react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
