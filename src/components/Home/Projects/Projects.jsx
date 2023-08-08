5305e7666e680a4f377bc086067cfd93e0d999e8
Masum Rahman <masumrahman73@gmail.com>
2023-08-08 22:53:01 +0600

    third project pic changed

--- src/components/Home/Projects/Projects.jsx
+++ src/components/Home/Projects/Projects.jsx
@@ -106,7 +106,7 @@ const Projects = () => {
                         className="m-0 rounded-none"
                     >
                         <img
-                            src="https://images.unsplash.com/photo-1556379118-7034d926d258?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1732&q=80"
+                            src="https://images.unsplash.com/photo-1621419203051-f4e463849784?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
                             alt="ui/ux review check"
                         />
                     </CardHeader>
@@ -114,7 +114,7 @@ const Projects = () => {
                         <Typography variant="h5" color="blue-gray" className="mb-2">
                             Musical Melody
                         </Typography>
-                        <Typography>
+                        <Typography className='h-32'>
                             Material Tailwind, React, React Route, Private Route, Firebase (Email-Password,Google), Axios, React Query, Sweet Alert 2 and many more.
                         </Typography>
                     </CardBody>
