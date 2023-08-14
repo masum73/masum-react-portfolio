8e2e7efb646a61ff2a8dd309be44a86293312023
Masum Rahman <masumrahman73@gmail.com>
2023-08-14 23:20:27 +0600

    5th card added

--- src/components/Home/Projects/Reviews.jsx
+++ src/components/Home/Projects/Reviews.jsx
@@ -305,6 +305,43 @@ const Reviews = () => {
                         </CardBody>
                     </Card>
                 </SwiperSlide>
+                <SwiperSlide>
+                    <Card color="gray" shadow={false} className="w-[26rem] px-8 py-5 mt-5 mb-16">
+                        <CardHeader
+                            color="transparent"
+                            floated={false}
+                            shadow={false}
+                            className="mx-0 flex items-center gap-4 pt-0 pb-8"
+                        >
+                            <Avatar
+                                size="lg"
+                                variant="circular"
+                                src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+                                alt="candice wu"
+                            />
+                            <div className="flex w-full flex-col gap-0.5">
+                                <div className="flex items-center justify-between">
+                                    <Typography variant="h5" color="blue-gray">
+                                        Wahid Sakib
+                                    </Typography>
+                                    <div className="5 flex items-center gap-0">
+                                        <StarIcon className="h-5 w-5 text-yellow-700" />
+                                        <StarIcon className="h-5 w-5 text-yellow-700" />
+                                        <StarIcon className="h-5 w-5 text-yellow-700" />
+                                        <StarIcon className="h-5 w-5 text-yellow-700" />
+                                        <StarIcon className="h-5 w-5 text-yellow-700" />
+                                    </div>
+                                </div>
+                                <Typography color="blue-gray">Software Engineer @ ReliSource</Typography>
+                            </div>
+                        </CardHeader>
+                        <CardBody className="mb-6 p-0">
+                            <Typography>
+                            I really liked that you made the effort to keep every team member up to date on your tasks. It really helps the team make better informed decisions and it shows great collaboration. Working well together is a great part of our company culture and you really understood the task.
+                            </Typography>
+                        </CardBody>
+                    </Card>
+                </SwiperSlide>
                 {/* <SwiperSlide>Slide 4</SwiperSlide>
                 <SwiperSlide>Slide 5</SwiperSlide>
                 <SwiperSlide>Slide 6</SwiperSlide>
