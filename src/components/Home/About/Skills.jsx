be095013aa96ad1a9e72a7cc357b510c34c5614d
Masum Rahman <masumrahman73@gmail.com>
2023-08-22 20:11:52 +0600

    skills page unwanted comments removed

--- src/components/Home/About/Skills.jsx
+++ src/components/Home/About/Skills.jsx
@@ -138,13 +138,7 @@ const Skills = () => {
             label: "Back-End",
             value: "back-end",
             desc: <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
-                {/* <div className="w-full">
-                    <div className="flex items-center justify-between gap-4 mb-2">
-                        <Typography color="Black" variant="h6">NPM</Typography>
-                        <Typography color="indigo" variant="h6">70%</Typography>
-                    </div>
-                    <Progress value={70} />
-                </div> */}
+                
                 <div className="w-full">
                     <div className="flex items-center justify-between gap-4 mb-2">
                         <Typography color="Black" variant="h6">Node JS</Typography>
@@ -173,20 +167,7 @@ const Skills = () => {
                     </div>
                     <Progress value={90} />
                 </div>
-                {/* <div className="w-full">
-                <div className="flex items-center justify-between gap-4 mb-2">
-                    <Typography color="Black" variant="h6">Netlify</Typography>
-                    <Typography color="indigo" variant="h6">85%</Typography>
-                </div>
-                <Progress value={85} />
-            </div>
-            <div className="w-full">
-                <div className="flex items-center justify-between gap-4 mb-2">
-                    <Typography color="Black" variant="h6">Vercel</Typography>
-                    <Typography color="indigo" variant="h6">85%</Typography>
-                </div>
-                <Progress value={85} />
-            </div> */}
+               
                 <div className="w-full">
                     <div className="flex items-center justify-between gap-4 mb-2">
                         <Typography color="Black" variant="h6">JWT</Typography>
