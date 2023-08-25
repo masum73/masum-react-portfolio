9370e654f2405469bdd58733c0d36a1134aaf62d
Masum Rahman <masumrahman73@gmail.com>
2023-08-25 22:32:16 +0600

    removed comment from about page

--- src/components/Home/About/About.jsx
+++ src/components/Home/About/About.jsx
@@ -4,14 +4,14 @@ import React from 'react';
 const About = () => {
     return (
         <div id='about' className='mx-auto w-9/12 pt-12'>
-            {/* <h2 className='text-3xl font-semibold'>About Me</h2> */}
+           
             <Typography className='my-5 text-center' variant="h2" color="blue" textGradient>
                 About Me
             </Typography>
             <Typography className='text-center' variant="lead" color="blue-gray">
                 Hey, I'm a SQA engineer by profession with a deep passion for web development.
             </Typography>
-            {/* <br /> <br /> */}
+
             <Typography className='my-5' variant="paragraph">
                 Throughout my career, I have acquired extensive knowledge and hands-on experience in software quality assurance, ensuring that products meet the highest standards of functionality, performance, and usability. I find great satisfaction in uncovering bugs, conducting thorough testing, and collaborating with developers to enhance the overall quality of software systems.
             </Typography>
